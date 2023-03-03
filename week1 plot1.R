842c869d82a1d3aa4f8fdbb28c7fa8af87589505
mrjahnavi <126406391+mrjahnavi@users.noreply.github.com>
2023-03-03 13:31:28 +0530

    Update week1 plot1.R

--- week1 plot1.R
+++ week1 plot1.R
@@ -1,14 +1,31 @@
-# Read in the household power consumption data from a CSV file
-power_data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ';', 
-                       na.strings = "?", nrows = 2075259, check.names = FALSE, 
-                       stringsAsFactors = FALSE, comment.char = "", quote = '\"')
-
-# Select data for 1/2/2007 and 2/2/2007
-selected_data <- subset(power_data, Date %in% c("1/2/2007", "2/2/2007"))
-
-# Convert the date column to a date object
-selected_data$Date <- as.Date(selected_data$Date, format = "%d/%m/%Y")
-
-# Create a histogram of the global active power variable for the selected dates
-hist(selected_data$Global_active_power, main = "Global Active Power", 
-     xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+plot1<- function(){
+        ## Aim of this function is to 
+        ## 1. read the household_power_consumption.txt file
+        ## 2. subset for data taken from 2 days: 2007-02-01 and 2007-02-02
+        ## 3. generate a histogram of global active power(kilowatts)
+        
+        ## Parameters: none
+        ## Assumes household_power_consumption.txt file located in working dir
+        
+        ## read data
+        powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+        
+        ## change class of all columns to correct class
+        powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+        powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
+        powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
+        powerdata$Global_reactive_power <- as.numeric(powerdata$Global_reactive_power)
+        powerdata$Voltage <- as.numeric(powerdata$Voltage)
+        powerdata$Global_intensity <- as.numeric(powerdata$Global_intensity)
+        powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1)
+        powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
+        powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
+        
+        ## subset data from 2007-02-01 and 2007-02-02
+        subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
+        
+        ## plot histogram of global active power for those 2 days
+        png("plot1.png", width=480, height=480)
+        hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+        dev.off()
+}
